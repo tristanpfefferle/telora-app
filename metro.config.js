9b9eb09c4968a62619f0a4749da150914b2df263
Tristan Pfefferlé <tristan.pfefferle@protonmail.com>
2026-04-15 05:40:58 +0000

    fix: use nativewind/babel as preset (not plugin) for NativeWind v4

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,5 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
-// NativeWind configuration
-module.exports = withNativeWind(config, {
-  input: './global.css',
-});
+module.exports = config;
