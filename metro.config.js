fe9a4db7172672e0c3a4e6310e5065534094f922
Tristan Pfefferlé <tristan.pfefferle@protonmail.com>
2026-04-15 05:15:18 +0000

    fix: use withNativeWind metro wrapper for NativeWind v4

--- metro.config.js
+++ metro.config.js
@@ -1,14 +1,9 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
-// Ensure Metro resolves modules from the correct directories
-config.resolver.nodeModulesPaths = [
-  __dirname,
-  __dirname + '/node_modules',
-];
-
-// Watch the mobile directory only
-config.watchFolders = [__dirname];
-
-module.exports = config;
+// NativeWind configuration
+module.exports = withNativeWind(config, {
+  input: './global.css',
+});
